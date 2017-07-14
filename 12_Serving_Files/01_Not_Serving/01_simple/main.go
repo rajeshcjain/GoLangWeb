417393871a22d397e039fdc8920d38dfbbee5d06
Jain <Rajesh.Jain@aristocrat.com>
2017-07-14 19:21:19 +0530

    commit

---
+++ 12_Serving_Files/01_Not_Serving/01_simple/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	//"fmt"
+	"io"
+)
+
+
+func handleReq(w http.ResponseWriter, req *http.Request){
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	//fmt.Fprint(w,`<img>https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg</img>`)
+	io.WriteString(w, `
+	<!--not serving from our server-->
+	<img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg">
+	`)
+
+
+}
+
+
+func main() {
+	http.HandleFunc("/img",handleReq)
+	http.ListenAndServe(":7777",nil)
+}
